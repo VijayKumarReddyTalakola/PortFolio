40611242e235a2907886df6e86a14ecd598368e6
VijayKumarReddyTalakola <109469574+VijayKumarReddyTalakola@users.noreply.github.com>
2023-04-10 21:31:34 +0530

    Added some Projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
 import html from '../../assets/Images/html.png'
+import imdb from '../../assets/Images/IMDB.png'
+import hindu from '../../assets/Images/theHindu.png'
 import './Projects.css'
 
 const Projects = () => {
@@ -15,21 +17,48 @@ const Projects = () => {
           <img src={html} alt="" />
           <div className="project-desc">
             <h3>Portfolio Website</h3>
-            <p> This is a website which provides all the basic info about me.</p>
+            <p>
+              {" "}
+              This is a website which provides all the basic info about me.
+            </p>
             <div className="project-links">
-              <a href="https://github.com/VijayKumarReddyTalakola/Port_Folio" target="_blank" rel="noreferrer"><FaGithub /></a>
-              <a href="https://vijaykumar-portfolio.vercel.app" target="_blank" rel="noreferrer"><FaExternalLinkAlt /></a>
+              <a
+                href="https://github.com/VijayKumarReddyTalakola/Port_Folio"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaGithub />
+              </a>
+              <a
+                href="https://vijaykumar-portfolio.vercel.app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaExternalLinkAlt />
+              </a>
             </div>
           </div>
         </div>
         <div className="project">
-          <img src={html} alt="" />
+          <img src={imdb} alt="" />
           <div className="project-desc">
             <h3>IMDB Website</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-            <div className="project-links"> 
-              <a href="https://github.com/VijayKumarReddyTalakola/Port_Folio" target="_blank" rel="noreferrer"><FaGithub /></a>
-              <a href="https://vijaykumar-portfolio.vercel.app" target="_blank" rel="noreferrer"><FaExternalLinkAlt /></a>
+            <p>A simple movie website created using React and TMDB Api</p>
+            <div className="project-links">
+              <a
+                href="https://github.com/VijayKumarReddyTalakola/IMDB"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaGithub />
+              </a>
+              <a
+                href="https://imdb-rho.vercel.app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaExternalLinkAlt />
+              </a>
             </div>
           </div>
         </div>
@@ -39,8 +68,21 @@ const Projects = () => {
             <h3>Ecomm Website</h3>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
             <div className="project-links">
-              <a href="https://github.com/VijayKumarReddyTalakola/Port_Folio" target="_blank" rel="noreferrer"> <FaGithub /></a>
-              <a href="https://vijaykumar-portfolio.vercel.app" target="_blank" rel="noreferrer"><FaExternalLinkAlt /></a>
+              <a
+                href="https://github.com/VijayKumarReddyTalakola/Port_Folio"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {" "}
+                <FaGithub />
+              </a>
+              <a
+                href="https://vijaykumar-portfolio.vercel.app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaExternalLinkAlt />
+              </a>
             </div>
           </div>
         </div>
@@ -48,21 +90,52 @@ const Projects = () => {
           <img src={html} alt="" />
           <div className="project-desc">
             <h3>Portfolio Website</h3>
-            <p> This is a portfolio website which provides all the basic info about me.</p>
+            <p>
+              {" "}
+              This is a portfolio website which provides all the basic info
+              about me.
+            </p>
             <div className="project-links">
-              <a href="https://github.com/VijayKumarReddyTalakola/Port_Folio" target="_blank" rel="noreferrer"><FaGithub /></a>
-              <a href="https://vijaykumar-portfolio.vercel.app" target="_blank" rel="noreferrer"><FaExternalLinkAlt /></a>
+              <a
+                href="https://github.com/VijayKumarReddyTalakola/Port_Folio"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaGithub />
+              </a>
+              <a
+                href="https://vijaykumar-portfolio.vercel.app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaExternalLinkAlt />
+              </a>
             </div>
           </div>
         </div>
         <div className="project">
-          <img src={html} alt="" />
+          <img src={hindu} alt="" />
           <div className="project-desc">
-            <h3>IMDB Website</h3>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+            <h3>TheHindu Clone</h3>
+            <p>
+               The TheHindu E-Paper Landing Page using HTML
+              and CSS.
+            </p>
             <div className="project-links">
-              <a href="https://github.com/VijayKumarReddyTalakola/Port_Folio" target="_blank" rel="noreferrer"><FaGithub /></a>
-              <a href="https://vijaykumar-portfolio.vercel.app" target="_blank" rel="noreferrer"><FaExternalLinkAlt /></a>
+              <a
+                href="https://github.com/VijayKumarReddyTalakola/theHindu-Clone"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaGithub />
+              </a>
+              <a
+                href="https://thehindu-epaper-clone.netlify.app"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaExternalLinkAlt />
+              </a>
             </div>
           </div>
         </div>
